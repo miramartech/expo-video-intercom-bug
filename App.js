c954483c4ba23cb384ed7daf4f1410adb9dfe8cf
Zeff Svoboda <zeff@miramar.tech>
2025-08-21 18:35:33 -0500

    Initial project with bug.

---
+++ App.js
@@ -0,0 +1,62 @@
+import { useEvent } from "expo";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { StyleSheet, View, Button, Text } from "react-native";
+
+const videoSource =
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+export default function App() {
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+    player.play();
+  });
+
+  const { isPlaying } = useEvent(player, "playingChange", {
+    isPlaying: player.playing,
+  });
+
+  const { error } = useEvent(player, "statusChange", player);
+
+  return (
+    <View style={styles.contentContainer}>
+      <VideoView
+        style={styles.video}
+        player={player}
+        allowsFullscreen
+        allowsPictureInPicture
+      />
+      <View style={styles.controlsContainer}>
+        <Button
+          title={isPlaying ? "Pause" : "Play"}
+          onPress={() => {
+            if (isPlaying) {
+              player.pause();
+            } else {
+              player.play();
+            }
+          }}
+        />
+      </View>
+      {error && (
+        <Text style={{ backgroundColor: "white" }}>{error.message}</Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 50,
+  },
+  video: {
+    width: 350,
+    height: 275,
+  },
+  controlsContainer: {
+    padding: 10,
+  },
+});
